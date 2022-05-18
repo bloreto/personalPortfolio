b2890ff8026c3c1cb9456ae6d7adb09e7efec809
Ben <bloreto@asu.edu>
2022-05-17 17:01:35 -0700

    Added App and HomeBanner

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">App comp</div>
+    );
+}
+
+export default App;
---
+++ src/components/HomeBannerTitle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../index.css'
+
+const HomeBannerTitle = () => {
+    return (
+        <h1 className="HomeBannerTitle"> Hi, I'm Ben.</h1>
+    );
+}
+
+export default HomeBannerTitle;
+
